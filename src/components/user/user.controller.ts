f8c963c75eed6a219c2c6914ac1e3ce307498eb9
viveksharma <viveksharma48b@gmail.com>
2023-08-26 21:52:20 +0530

    TO File

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -1,26 +1,29 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import axios from 'axios';
+import fs from 'fs';
 
 const getImage = async (req: Request, res: Response) => {
   try {
     let imageURL = req.params.image;
     imageURL =
-      'https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-xCICeBcjmKHkR2vvY2kadKmvSR7lnoDhqz5t7gL3mRTKitLHBtuAHuxQe-ICZfwxSiFYohdHcH9shA5iMmiQYmQVjF=w1365-h970';
+      'https://www.googleapis.com/drive/v3/files/1WVY17oVcZ92CFyGHD6PdNJIZHvgv2fHE?alt=media';
     console.log('ImageURL', imageURL);
     let image: any = await axios.get(imageURL, {
       responseType: 'arraybuffer',
       headers: {
+        'X-goog-api-key': 'AIzaSyCr7yZnMnaY1I_hXisTqy7DPtDCK1eE_YU',
+        'Content-Type': 'application/json; charset=utf-8',
         Accept: 'media',
         cors: 'no-cors',
         origin: 'local',
-      },
+      },  
     });
-    console.log(image);
+    const img = fs.writeFileSync('temp.png', image.data);
+    console.log(typeof image.data, image.data);
     const { data } = image;
-    console.log('UInt8Array', data);
     image = image.data;
-    // image = new Uint8Array(image);
+    image = new Uint8Array(image);
     res.status(httpStatus.OK);
     res.send(image);
   } catch (e) {
@@ -30,4 +33,5 @@ const getImage = async (req: Request, res: Response) => {
   }
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export { getImage };
